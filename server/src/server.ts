81c08187e592ab0c45510927dac39b3609311295
Musa Musa Kannike <156730039+musamusakannike@users.noreply.github.com>
2026-02-04 17:28:30 +0100

    removed cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -39,12 +39,7 @@ app.set("io", io)
 
 // Middleware
 app.use(helmet())
-app.use(
-  cors({
-    origin: "*",
-    credentials: true,
-  }),
-)
+app.use(cors())
 app.use(morgan("dev"))
 app.use(compression())
 app.use(express.json({ limit: "10mb" }))
